ef5a5e837d9df92e39fb24e478b35a7450ae2033
Jonathan Hsu <bit172@gmail.com>
2018-05-11 23:52:13 -0400

    Insertion Sort Done

--- ISort/src/ISort.java
+++ ISort/src/ISort.java
@@ -2,14 +2,42 @@
 public class ISort {
 
 	public static void main(String[] args) {
-		int[] numbers;
-		
-		for (int i = 0; i < args.length; i++) 
+		int length = args.length;
+		int[] numbers = new int[length];
+
+		for (int i = 0; i < length; i++) 
 		{
-			
-
+			numbers[i] = Integer.parseInt(args[i]);
 		}
+		
+		InsertionSort(numbers);
+		printArray(numbers);
 
+	}
+	
+	public static void printArray(int[] array) 
+	{
+		int l = array.length;
+		for(int i = 0; i < l;i++)
+		System.out.print(array[i] + " ");
+	}
+	
+	public static void InsertionSort(int[] array) 
+	{	
+		int l = array.length;
+		for (int i = 1; i < l; i++) // We ignore the first element of the array since we consider it 'sorted'
+		{	
+			int element_to_insert = array[i];
+			int empty_position = i;
+			
+			while(empty_position > 0 && array[empty_position-1] > element_to_insert)
+			{
+				array[empty_position] = array[empty_position-1];
+				empty_position--;
+			}
+			
+			array[empty_position] = element_to_insert;
+		} 	
 	}
 
 }
