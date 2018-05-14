7ff92de59ebe422f33bbfe1c2b959201f8a4a61b
Jonathan Hsu <bit172@gmail.com>
2018-05-14 09:14:18 -0400

    Debug

--- ISort/src/ISort.java
+++ ISort/src/ISort.java
@@ -2,42 +2,52 @@
 public class ISort {
 
 	public static void main(String[] args) {
-		int length = args.length;
-		int[] numbers = new int[length];
+		int length;
+		int[] numbers;
 
-		for (int i = 0; i < length; i++) 
-		{
-			numbers[i] = Integer.parseInt(args[i]);
+		if (args[0].equals("debug")) {
+			length = args.length;
+			numbers = new int[length - 1];
+
+			for (int i = 1, j = 0; i < length; i++, j++)
+				numbers[j] = Integer.parseInt(args[i]);
+
+			InsertionSortDebug(numbers);
+		} else {
+
+			length = args.length;
+			numbers = new int[length];
+
+			for (int i = 0; i < length; i++)
+				numbers[i] = Integer.parseInt(args[i]);
+
+			InsertionSort(numbers);
+
 		}
-		
-		InsertionSort(numbers);
-		
 
 	}
-	
-	public static void printArray(int[] array) 
-	{
+
+	public static void printArray(int[] array) {
 		int l = array.length;
-		for(int i = 0; i < l;i++)
-		System.out.print(array[i] + " ");
+		for (int i = 0; i < l; i++)
+			System.out.print(array[i] + " ");
 	}
-	
-	public static void InsertionSort(int[] array) 
-	{	
+
+	public static void InsertionSort(int[] array) {
 		int l = array.length;
 		long start_time = System.nanoTime();
 		for (int i = 1; i < l; i++) // We ignore the first element of the array since we consider it 'sorted'
-		{	
+		{
 			int element_to_insert = array[i];
-			int empty_position = i;
-			
-			while(empty_position > 0 && array[empty_position-1] > element_to_insert)
+			int empty_position = i - 1;
+
+			while (empty_position >= 0 && array[empty_position] > element_to_insert) // element compared is greater
 			{
-				array[empty_position] = array[empty_position-1];
+				array[empty_position + 1] = array[empty_position]; // shifting elements if they are greater than the current element
 				empty_position--;
 			}
-			
-			array[empty_position] = element_to_insert;
+			// element compared is smaller
+			array[empty_position + 1] = element_to_insert;
 		}
 		long end_time = System.nanoTime();
 		long run_time = end_time - start_time;
@@ -45,4 +55,38 @@ public class ISort {
 		System.out.println("\ncompleted in " + run_time + "ns");
 	}
 
-}
+	public static void InsertionSortDebug(int[] array) {
+		int l = array.length;
+
+		long start_time = System.nanoTime();
+		// First item is considered sorted
+		for (int i = 0; i < l; i++) {
+			if (i == 0)
+				System.out.print("[" + array[i] + "]" + " ");
+			else
+				System.out.print(array[i] + " ");
+		}
+		System.out.print("|\n");
+
+		for (int i = 1; i < l; i++) // We ignore the first element of the array since we consider it 'sorted'
+		{
+			int element_to_insert = array[i];
+			int empty_position = i - 1;
+			
+			while(empty_position >= 0)
+			{
+				
+				
+			}
+			
+			
+		}
+		long end_time = System.nanoTime();
+		long run_time = end_time - start_time;
+		printArray(array);
+		System.out.println("\ncompleted in " + run_time + "ns");
+
+		}
+	}
+
+
